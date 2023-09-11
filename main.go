c8c364884574527493fa3ff289a86b86fc97407a
Farzad FARID <farzy@farzy.org>
2023-09-11 21:48:10 +0200

    Arguments with conditional

--- main.go
+++ main.go
@@ -7,5 +7,9 @@ import (
 
 func main() {
 	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./golang-for-devops <argument>\n")
+		os.Exit(1)
+	}
 	fmt.Printf("Hello World!\nos.Args: %v\nArgument: %v\n", args, args[1:])
 }
