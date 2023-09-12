497c17a846d9b799efbbaafed9baf6c49b089bf6
Farzad FARID <farzy@farzy.org>
2023-09-12 10:59:11 +0200

    Write an HTTP API client

--- main.go
+++ main.go
@@ -2,14 +2,37 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
 	args := os.Args
 	if len(args) < 2 {
-		fmt.Printf("Usage: ./golang-for-devops <argument>\n")
+		fmt.Printf("Usage: ./http-get <url>\n")
 		os.Exit(1)
 	}
-	fmt.Printf("Hello World!\nos.Args: %v\nArgument: %v\n", args, args[1:])
+	myUrl := args[1]
+	if _, err := url.ParseRequestURI(myUrl); err != nil {
+		fmt.Printf("URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(myUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status code: %d\nBody: %s\n", response.StatusCode, body)
+	// fmt.Printf("HTTP Status code: %d\nBody: %v\n", response.StatusCode, string(body)) // Explicit []byte to string conversion
 }
