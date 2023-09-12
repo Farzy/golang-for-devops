708ed85c25334778d1120bb730fe113d084bc79c
Farzad FARID <farzy@farzy.org>
2023-09-12 16:38:23 +0200

    Parse HTTP JSON response

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
+
+// Words structure
+// Example json: {"page":"words","input":"word1","words":["word3","word2","word1"]}
+type Words struct {
+	Page  string   `json:"page"`
+	Input string   `json:"input"`
+	Words []string `json:"words"`
+}
 
 func main() {
 	args := os.Args
@@ -35,4 +45,18 @@ func main() {
 
 	fmt.Printf("HTTP Status code: %d\nBody: %s\n", response.StatusCode, body)
 	// fmt.Printf("HTTP Status code: %d\nBody: %v\n", response.StatusCode, string(body)) // Explicit []byte to string conversion
+
+	if response.StatusCode != http.StatusOK {
+		fmt.Printf("Invalid output (HTTP code: %d): %s\n", response.StatusCode, body)
+		os.Exit(1)
+	}
+
+	var words Words
+
+	err = json.Unmarshal(body, &words)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("JSON Parsed\nPage: %s\nWords: %v\n", words.Page, strings.Join(words.Words, ", "))
 }
