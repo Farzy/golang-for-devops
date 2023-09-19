0a59fc38fdf8ef5d5d9215117bff496869ff7fe5
Farzad FARID <farzy@farzy.org>
2023-09-19 16:56:15 +0200

    Wait for 2 threads using WaitGroup

--- cmd/go-demo/main.go
+++ cmd/go-demo/main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
+	wg := sync.WaitGroup{}
 	fmt.Printf("one\n")
-	c := make(chan string)
-	c2 := make(chan string)
-	go testFunction("Thread 1", 300*time.Millisecond, c)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		testFunction("Thread 1", 300*time.Millisecond)
+	}()
 	fmt.Printf("two\n")
-	go testFunction("Thread 2", 800*time.Millisecond, c2)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		testFunction("Thread 2", 800*time.Millisecond)
+	}()
 	fmt.Printf("three\n")
-	areWeFinished := []string{<-c, <-c2}
-	fmt.Printf("areWeFinished: %v\n", areWeFinished)
+	wg.Wait()
+	fmt.Printf("We are finished!\n")
 }
 
-func testFunction(id string, pause time.Duration, c chan string) {
+func testFunction(id string, pause time.Duration) {
 	for i := 0; i < 5; i++ {
 		fmt.Printf("#%s: checking…\n", id)
 		time.Sleep(pause)
 	}
-	c <- fmt.Sprintf("#%s: we are finished", id)
 }
