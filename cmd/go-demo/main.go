adb544e61bd79ad78013dffbc9b61949c516cfe0
Farzad FARID <farzy@farzy.org>
2023-09-19 18:16:48 +0200

    Add a call to Once

--- cmd/go-demo/main.go
+++ cmd/go-demo/main.go
@@ -8,26 +8,31 @@ import (
 
 func main() {
 	wg := sync.WaitGroup{}
+	var once sync.Once
+
 	fmt.Printf("one\n")
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		testFunction("Thread 1", 300*time.Millisecond)
+		testFunction("Thread 1", 300*time.Millisecond, &once)
 	}()
-	time.Sleep(500 * time.Millisecond)
 	fmt.Printf("two\n")
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		testFunction("Thread 2", 800*time.Millisecond)
+		testFunction("Thread 2", 800*time.Millisecond, &once)
 	}()
 	fmt.Printf("three\n")
 	wg.Wait()
 	fmt.Printf("We are finished!\n")
 }
 
-func testFunction(id string, pause time.Duration) {
+func testFunction(id string, pause time.Duration, once *sync.Once) {
 	for i := 0; i < 5; i++ {
+		once.Do(func() {
+			fmt.Printf("#%s go the Once call\n", id)
+		})
+
 		fmt.Printf("#%s: checking…\n", id)
 		time.Sleep(pause)
 	}
