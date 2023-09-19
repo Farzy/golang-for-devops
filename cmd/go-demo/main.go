419cda17266fd3a1662a33a25a94f568cf7cb798
Farzad FARID <farzy@farzy.org>
2023-09-19 16:51:57 +0200

    Wait for 2 threads using channels

--- cmd/go-demo/main.go
+++ cmd/go-demo/main.go
@@ -8,16 +8,19 @@ import (
 func main() {
 	fmt.Printf("one\n")
 	c := make(chan string)
-	go testFunction(c)
+	c2 := make(chan string)
+	go testFunction("Thread 1", 300*time.Millisecond, c)
 	fmt.Printf("two\n")
-	areWeFinished := <-c
+	go testFunction("Thread 2", 800*time.Millisecond, c2)
+	fmt.Printf("three\n")
+	areWeFinished := []string{<-c, <-c2}
 	fmt.Printf("areWeFinished: %v\n", areWeFinished)
 }
 
-func testFunction(c chan string) {
+func testFunction(id string, pause time.Duration, c chan string) {
 	for i := 0; i < 5; i++ {
-		fmt.Printf("checking…\n")
-		time.Sleep(1 * time.Second)
+		fmt.Printf("#%s: checking…\n", id)
+		time.Sleep(pause)
 	}
-	c <- "we are finished"
+	c <- fmt.Sprintf("#%s: we are finished", id)
 }
