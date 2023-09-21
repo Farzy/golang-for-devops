64ca15acb2949044dd77d7503dfc7b84fa9c41b3
Farzad FARID <farzy@farzy.org>
2023-09-21 13:38:49 +0200

    Fix code with a bucket constraint

--- cmd/aws-demo-s3/main.go
+++ cmd/aws-demo-s3/main.go
@@ -6,9 +6,11 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"os"
 )
 
+const region = "eu-west-3"
 const bucketName = "farzad-aws-demo-test-bucket-3976973"
 
 func main() {
@@ -19,12 +21,12 @@ func main() {
 	)
 
 	ctx := context.Background()
-	if s3Client, err = initS3Client(ctx, "eu-west-3"); err != nil {
+	if s3Client, err = initS3Client(ctx, region); err != nil {
 		fmt.Printf("initS3Client error: %s\n", err)
 		os.Exit(1)
 	}
 
-	if bucketOutput, err = createS3Bucket(ctx, s3Client); err != nil {
+	if bucketOutput, err = createS3Bucket(ctx, s3Client, region); err != nil {
 		fmt.Printf("createS3Bucket error: %s\n", err)
 		os.Exit(1)
 	}
@@ -41,9 +43,12 @@ func initS3Client(ctx context.Context, region string) (*s3.Client, error) {
 	return s3.NewFromConfig(cfg), nil
 }
 
-func createS3Bucket(ctx context.Context, s3Client *s3.Client) (*s3.CreateBucketOutput, error) {
+func createS3Bucket(ctx context.Context, s3Client *s3.Client, region string) (*s3.CreateBucketOutput, error) {
 	bucket, err := s3Client.CreateBucket(ctx, &s3.CreateBucketInput{
 		Bucket: aws.String(bucketName),
+		CreateBucketConfiguration: &types.CreateBucketConfiguration{
+			LocationConstraint: types.BucketLocationConstraint(region),
+		},
 	})
 	if err != nil {
 		return nil, fmt.Errorf("unable to create bucket: %s", err)
