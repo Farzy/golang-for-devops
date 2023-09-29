e9eb23bde4d7c5b0b5ddf9574e9225798ca33325
Farzad FARID <farzy@farzy.org>
2023-09-29 20:33:03 +0200

    Add a logger middleware

--- cmd/http-mux/main.go
+++ cmd/http-mux/main.go
@@ -34,6 +34,24 @@ func CurrentTimeHandler(w http.ResponseWriter, _ *http.Request) {
 	_, _ = w.Write([]byte(fmt.Sprintf("The current time is %v\n", curTime)))
 }
 
+// Logger is a middleware handler that does request logging
+type Logger struct {
+	handler http.Handler
+}
+
+// ServeHTTP handles the request by passing it to the real
+// handler and logging the request details
+func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	l.handler.ServeHTTP(w, r)
+	log.Printf("Logger: %s %s %v", r.Method, r.URL.Path, time.Since(start))
+}
+
+// NewLogger constructs a new Logger middleware handler
+func NewLogger(next http.Handler) *Logger {
+	return &Logger{next}
+}
+
 func main() {
 	addr, exists := os.LookupEnv("ADDR")
 	if !exists {
@@ -44,7 +62,8 @@ func main() {
 	for _, handler := range handlers {
 		mux.HandleFunc(handler.path, handler.fn)
 	}
+	wrappedMux := NewLogger(mux)
 
 	log.Printf("Server is listening at %s", addr)
-	log.Fatal(http.ListenAndServe(addr, mux))
+	log.Fatal(http.ListenAndServe(addr, wrappedMux))
 }
