e7e8188e5173ee1ccfac794bf333e302b6e72065
Farzad FARID <farzy@farzy.org>
2023-10-04 14:27:01 +0200

    Provide a clear guide to convert "requests per second" into MaxToken and Rate

--- pkg/mux/token-bucket/main.go
+++ pkg/mux/token-bucket/main.go
@@ -1,6 +1,32 @@
 // Package token_bucket implements a simple rate limiter using the Token Bucket algorithm.
 // The Token Bucket algorithm is commonly used for traffic shaping and rate limiting.
 // This implementation is adapted from https://himanshu-007.medium.com/simple-rate-limiter-in-golang-using-token-bucket-algorithm-388d0596d1e4.
+//
+// Here's a way to convert requests/sec into MaxTokens and Rate, provided by Google Bard:
+// To convert "requests per second" into MaxToken and Rate in the Token Bucket algorithm, you can use the following formulas:
+//
+// ```
+// MaxToken = requests per second * burst interval
+// Rate = requests per second / burst interval
+// ```
+//
+// Where:
+//
+// * **burst interval** is the number of seconds that the system allows to burst at the maximum rate.
+//
+// For example, if you want to allow a maximum of 100 requests per second with a burst interval of 5 seconds, then your MaxToken would be 500 and your Rate would be 20.
+//
+// ```
+// MaxToken = 100 requests per second * 5 seconds = 500
+// Rate = 100 requests per second / 5 seconds = 20
+// ```
+//
+// This means that the system would allow a maximum of 500 requests in a 5-second period, and then it would start limiting requests to 20 per second.
+//
+// Note that the burst interval is a trade-off between performance and fairness. A longer burst interval will allow the system to handle sudden spikes in traffic better, but it will also allow users to send more requests in a short period of time. A shorter burst interval will be fairer to all users, but it may also impact performance during sudden spikes in traffic.
+//
+// You can adjust the MaxToken and Rate parameters to achieve the desired rate limiting behavior for your web API.
+
 package token_bucket
 
 import (
